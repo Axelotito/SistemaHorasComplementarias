3f14905e7d3fdb7abdecda7a9c75845bcfeb2291
Axelotito <axelinoxx1@gmail.com>
2026-05-13 22:01:35 -0600

    feat: add logout functionality to Sidebar and integrate with StudentDashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import StudentDashboard from './view/studentDashboard/studentDashboard';
-import LoginView from './view/loginView/LoginView';
-import ValidatorPanel from './view/validatorPanel/ValidatorPanel'; // Lo crearemos ahora
 
 function App() {
-  const [estaAutenticado, setEstaAutenticado] = useState(false);
-  const [rol, setRol] = useState('estudiante'); // 'estudiante' o 'profesor'
-
-  const iniciarSesion = (cuenta) => {
-    // Simulación: si la cuenta empieza con 'profe', entra como validador
-    if (cuenta.startsWith('profe')) {
-      setRol('profesor');
-    } else {
-      setRol('estudiante');
-    }
-    setEstaAutenticado(true);
-  };
-
-  const cerrarSesion = () => {
-    setEstaAutenticado(false);
-  };
-
   return (
     <div className="App">
-      {!estaAutenticado ? (
-        <LoginView onLogin={iniciarSesion} />
-      ) : (
-        // Pasamos la función cerrarSesion a ambos portales
-        rol === 'estudiante' ? (
-          <StudentDashboard onLogout={cerrarSesion} />
-        ) : (
-          <ValidatorPanel onLogout={cerrarSesion} />
-        )
-      )}
+      <StudentDashboard />
     </div>
   );
 }
